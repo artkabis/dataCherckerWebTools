62e75e6a4b5129504be89224cac82a7aafeefd75
Artkabis <artkabis@live.fr>
2023-08-07 08:36:15 +0200

    add filter excludes class ->alt check

---
+++ globalCherckerWebSiteCQ _Firerox/Functions/checkBold.js
@@ -0,0 +1,186 @@
+(($) => {
+  const strongOrBold = $(
+    "#Content b, #Content strong, #Content STRONG, #dm_content b, #dm_content B, #dm_content strong, #dm_content STRONG"
+  );
+  strongOrBold &&
+    console.log(
+      "----------------------------- Start Check strong & bold valitidy --------------------------------------------"
+    );
+  let cmpBold = 0,
+    boldArray = [],
+    isSlide = false;
+  strongOrBold.each(function (i, t) {
+    const isHnClosest =
+      $(this)[0].tagName.toLowerCase() === "h1" ||
+      $(this).parent()[0].tagName.toLowerCase() === "h1" ||
+      $(this)[0].tagName.toLowerCase() === "h2" ||
+      $(this).parent()[0].tagName.toLowerCase() === "h2" ||
+      $(this)[0].tagName.toLowerCase() === "h3" ||
+      $(this).parent()[0].tagName.toLowerCase() === "h3" ||
+      $(this)[0].tagName.toLowerCase() === "h4" ||
+      $(this).parent()[0].tagName.toLowerCase() === "h4" ||
+      $(this)[0].tagName.toLowerCase() === "h5" ||
+      $(this).parent()[0].tagName.toLowerCase() === "h5" ||
+      $(this)[0].tagName.toLowerCase() === "h6" ||
+      $(this).parent()[0].tagName.toLowerCase() === "h6";
+    isSlide = $(this).closest(".slide-inner");
+    if (t.textContent.length > 1 && t.textContent !== " ") {
+      if (!isHnClosest || !isSlide) {
+        cmpBold++;
+        boldArray.push({
+          target: t,
+          text: t.textContent,
+          nbWords: t.textContent.includes(" ")
+            ? t.textContent.split(" ").length
+            : 1,
+        });
+      }
+    }
+  });
+  $("#dm_content span").each(function (t) {
+    isSlide = $(this).closest(".slide-inner");
+    const isBold = (el) =>
+      el.attr("style") &&
+      (el.attr("style").includes("font-weight: bold") ||
+        (el.attr("style").includes("font-weight: 700") &&
+          $(this)[0].textContent.trim().length));
+    const isMultiSpan =
+      isBold($(this)) &&
+      isBold($(this).children()) &&
+      $(this)[0].textContent.trim().length
+        ? true
+        : false;
+    let target = isMultiSpan ? $(this).children() : $(this);
+    const isHnClosest =
+      target[0].tagName.toLowerCase() === "h1" ||
+      target.parent()[0].tagName.toLowerCase() === "h1" ||
+      target[0].tagName.toLowerCase() === "h3" ||
+      target.parent()[0].tagName.toLowerCase() === "h3" ||
+      target[0].tagName.toLowerCase() === "h2" ||
+      target.parent()[0].tagName.toLowerCase() === "h2" ||
+      target[0].tagName.toLowerCase() === "h4" ||
+      target.parent()[0].tagName.toLowerCase() === "h4" ||
+      target[0].tagName.toLowerCase() === "h5" ||
+      target.parent()[0].tagName.toLowerCase() === "h5" ||
+      target[0].tagName.toLowerCase() === "h6" ||
+      target.parent()[0].tagName.toLowerCase() === "h6";
+    const duplicateBold =
+      isMultiSpan &&
+      $(this)[0]
+        .textContent.trim()
+        .includes($(this).children()[0].textContent.trim());
+    isMultiSpan &&
+      !isHnClosest &&
+      console.log(
+        { target },
+        "  isBold : ",
+        isBold(target),
+        { isMultiSpan },
+        { duplicateBold },
+        "   text content",
+        target[0].textContent,
+        " text length",
+        $(this)[0].textContent.trim().length
+      );
+    isBold(target) &&
+      !isHnClosest &&
+      target[0].textContent !== "\n" &&
+      target[0].textContent !== "" &&
+      target[0].textContent.length > 1 &&
+      (boldArray.push({
+        target: target[0], // Modification : Ajouter [0] pour obtenir l'élément DOM
+        text: target[0].textContent.trim(),
+        nbWords: target[0].textContent.trim().split(" ").length,
+      }),
+      cmpBold++);
+    duplicateBold && cmpBold--;
+  });
+
+  // Créer un nouvel tableau pour stocker les éléments uniques
+  const objSansDoublons = [];
+
+  // Parcourir l'array initial boldArray
+  for (let i = 0; i < boldArray.length; i++) {
+    const element = boldArray[i];
+    const { target, text, nbWords } = element;
+    // Vérifier si les propriétés "text" et "nbWords" sont identiques
+    const isDuplicate = objSansDoublons.some(
+      (item) => item.text === text && item.nbWords === nbWords
+    );
+    if (!isDuplicate && text.length > 2 && !target.closest(".slide-inner")) {
+      objSansDoublons.push({
+        target, // Modification : Ne pas accéder à [0] pour conserver l'élément DOM
+        text,
+        nbWords,
+      });
+    }
+  }
+  dataChecker.bold_check.bold_txt = [];
+  dataChecker.bold_check.bold_check_state =
+    objSansDoublons.length === 0 || objSansDoublons === undefined
+      ? false
+      : true;
+  // dataChecker.bold_check.bold_check_state;
+  const isBoldValid =
+    objSansDoublons.length >= 3 && objSansDoublons.length <= 5;
+  !isBoldValid
+    ? console.log(
+        "%c Attention le nombre déléments mis en gras ne respect pas le standard (3 à 5 expressions), ici >>> " +
+          objSansDoublons.length,
+        "color:red"
+      )
+    : console.log(
+        "%c Le nombre déléments mis en gras respect le standard (3 à 5 expressions), ici >>> " +
+          objSansDoublons.length,
+        "color:green"
+      ),
+    console.log(objSansDoublons);
+
+  objSansDoublons.map((t) => {
+    dataChecker.bold_check.bold_check_state = true;
+    dataChecker.bold_check.bold_txt.push({
+      bold_txt_state: true,
+      bold_txt: t.text,
+      bold_nb_words: t.nbWords,
+    });
+
+    dataChecker.bold_check.nb_bold =
+      objSansDoublons.length && objSansDoublons.length;
+    //(isBoldValid) && objSansDoublons.length ? 5 : 0;
+  });
+
+  if (!objSansDoublons || objSansDoublons.length === 0) {
+    dataChecker.bold_check.bold_txt = [];
+  }
+  let scroreBold = 0;
+  let nbBold = dataChecker.bold_check.bold_txt.length
+    ? dataChecker.bold_check.bold_txt.length
+    : 0;
+  if (nbBold === 1) {
+    scroreBold = 1;
+  } else if (nbBold === 2) {
+    scroreBold = 2;
+  } else if (nbBold === 3) {
+    scroreBold = 3;
+  } else if (nbBold === 6) {
+    scroreBold = 4;
+  } else if (nbBold >= 3 && nbBold <= 5) {
+    scroreBold = 5;
+  } else if (nbBold === 7) {
+    scroreBold = 3;
+  } else if (nbBold === 8) {
+    scroreBold = 2;
+  } else if (nbBold === 9) {
+    scroreBold = 2;
+  } else if (nbBold === 10) {
+    scroreBold = 1;
+  } else if (nbBold < 1 && nbBold > 10) {
+    scroreBold = 0;
+  }
+  dataChecker.bold_check.global_score = scroreBold ? scroreBold : 0;
+  console.log({ nbBold }, { scroreBold });
+  cmpBold > 0 &&
+    console.log(
+      "----------------------------- End Check strong & bold valitidy --------------------------------------------"
+    );
+})(jQuery);
---
+++ globalCherckerWebSiteCQ _Firerox/Functions/dataCheckerSchema.js
@@ -0,0 +1,175 @@
+
+
+dataChecker = {
+    url_site: window.location.href,
+    global_score: "2",
+    state_check: "false",
+    cdp_global_score: {
+      check_title: "Global score CDP",
+      global_score: 0,
+      scores: [],
+    },
+    webdesigner_global_score: {
+      check_title: "Global score Webdesigner",
+      global_score: 0,
+      scores: [],
+    },
+    meta_check: {
+      meta_check_state: "false",
+      nb_meta: "2",
+      check_title: "Meta",
+      global_score: "3",
+      profil: ["CDP"],
+      meta: [
+        {
+          meta_state: "false",
+          meta_type: "title",
+          meta_txt: "mon title magique performant et beau",
+          meta_size: "6",
+          meta_reco: "La reco est de 50 à 65 caractères.",
+          meta_score: "5",
+          check_title: "Meta title",
+        },
+        {
+          meta_state: "false",
+          meta_type: "description",
+          meta_txt: "mon desc magique performant et beau",
+          meta_size: "6",
+          meta_reco: "la reco est de de 140 à 156 caractères.",
+          meta_score: "10",
+          check_title: "Meta description",
+        },
+      ],
+    },
+    link_check: {
+      link_check_state: "false",
+      nb_link: "1",
+      check_title: "Links validities",
+      global_score: "5",
+      profil: ["CDP", "WEBDESIGNER"],
+      link: [
+        {
+          link_state: "false",
+          link_url: "www.monimage.com",
+          link_status: 200,
+          link_text: "mon lien",
+          alt_img_score: "5",
+        },
+      ],
+    },
+    alt_img_check: {
+      alt_img_check_state: "false",
+      nb_alt_img: "1",
+      check_title: "Images alt",
+      global_score: "5",
+      profil: ["CDP"],
+      alt_img: [
+        {
+          alt_img_state: "true",
+          alt_img_src: "www.monimage.com",
+          alt_img_score: "5",
+        },
+      ],
+    },
+    img_check: {
+      img_check_state: "false",
+      nb_img: "1",
+      nb_img_duplicate: [],
+      check_title: "Images check",
+      global_score: "5",
+      profil: ["WEBDESIGNER"],
+      alt_img: [
+        {
+          alt_img_state: "true",
+          alt_img_src: "www.monimage.com",
+          alt_img_score: "5",
+          check_title: "Images alt",
+        },
+      ],
+      size_img: [
+        {
+          size_img_state: "false",
+          size_img_src: "www.monimage.com",
+          size_img: "20KB",
+          size_img_score: "5",
+          check_title: "Images size",
+        },
+      ],
+      ratio_img: [
+        {
+          ratio_img_state: "false",
+          ratio_img_src: "www.monimage.com",
+          type_img: "image/SRC",
+          img_height: "100px",
+          img_width: "100px",
+          parent_img_height: "100px",
+          parent_img_width: "100px",
+          ratio_parent_img_height: "100px",
+          ratio_parent_img_width: "100px",
+          ratio_img: "20KB",
+          ratio_img_score: "5",
+          check_title: "Images ratio",
+        },
+      ],
+    },
+    hn: {
+      hn_check_state: "false",
+      nb_hn: "1",
+      check_title: "Hn ",
+      global_score: "1",
+      hn_reco: {
+        profil: ["CDP"],
+        global_score: "1",
+        check_title: "Reco longueur Hn",
+        hn_preco: "La preco est de 50 à 90 caractères",
+        hn: [
+          {
+            hn_type: "h1",
+            hn_letters_count: "10",
+            hn_txt: "mon titre h1 est beau",
+            hn_index: "1",
+            hn_words_sliced: "5",
+            hn_words_count: "4",
+            hn_score: "5",
+            check_title: "Reco longueur des Hn",
+          },
+          {
+            hn_type: "h2",
+            hn_letters_count: "10",
+            hn_txt: "mon titre h1 est beau",
+            hn_index: "1",
+            hn_words_sliced: "5",
+            hn_words_count: "4",
+            hn_score: "5",
+            check_title: "Reco longueur des Hn",
+          },
+        ],
+      },
+      hn_outline: {
+        profil: ["CDP", "WEBDESIGNER"],
+        check_title: "Validité du outline des Hn",
+        global_score: "1",
+        hn: [
+          {
+            hn_type: "h2",
+            hn_validity: "true",
+            hn_validity_message: "Valide",
+          },
+        ],
+      },
+    },
+    bold_check: {
+      bold_check_state: "false",
+      nb_bold: "1",
+      check_title: "validité des préco lié au bold des textes.",
+      global_score: "0",
+      profil: ["CDP"],
+      bold_txt: [
+        {
+          bold_txt_state: "false",
+          bold_txt_content: "exmple texte en gras.",
+          bold_txt_score: "0",
+        },
+      ],
+    },
+  };
