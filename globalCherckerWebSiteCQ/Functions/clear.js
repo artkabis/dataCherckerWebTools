2c3a73b0479a9e75a402812b2ab417633a685906
Artkabis <artkabis@live.fr>
2023-12-08 13:08:54 +0100

    update start log

--- globalCherckerWebSiteCQ/Functions/clear.js
+++ globalCherckerWebSiteCQ/Functions/clear.js
@@ -1,6 +1,6 @@
 console.clear();
 console.log(
-    "%cHealth checker Website %c - tools %cby Greg ;)",
+    "%cChecker Website health %c - tools %cby Greg ;)",
     "color:#0097ff;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:bold",
     "color:#0097ff;font-family:system-ui;font-size:2rem;-webkit-text-stroke: 1px black;font-weight:bold",
     "color:#0097ff;font-family:system-ui;font-size:1rem;-webkit-text-stroke: 0.2px black;font-weight:bold"
